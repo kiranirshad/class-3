d4e29d69e42e8645e0adc91193ea63505ed11e66
kiran irshad <you@example.com>
2018-06-08 01:51:02 -0700

    initial git

---
+++ q15.js
@@ -0,0 +1,8 @@
+let weather = hot;
+speed = 14;
+
+if(weather! snowing weather ! raining && weather ! cloudy && speed<15){
+    console.log("The weather conditions are suitable for rocket");
+} else {
+    console.log("NOT");
+}
